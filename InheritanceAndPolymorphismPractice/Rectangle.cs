641596b09eb7a9e4da96383f39c6f523ff840f43
Weiskri <kkweis13@gmail.com>
2018-02-05 11:46:08 -0500

    first commit

---
+++ InheritanceAndPolymorphismPractice/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAndPolymorphismPractice
+{
+    public class Rectangle : Shape
+    {
+        private double area;
+
+        public double Area
+        {
+            get { return this.area; }
+            set { this.area = value; }
+        }
+        
+        public Rectangle(double area)
+        {
+            this.area = area;
+        }
+    }
+}
---
+++ InheritanceAndPolymorphismPractice/Shape.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAndPolymorphismPractice
+{
+    public class Shape
+    {
+        // fields
+        private int sides;
+
+        // properties
+
+        public int Sides
+        {
+            get { return this.sides; }
+            set { this.sides = value; }
+        }
+
+        // constructors
+        public Shape ()
+        {
+            // default constructor
+        }
+
+        public Shape(int sides)
+        {
+            this.sides = sides;
+        }
+
+        // methods
+    }
+}
